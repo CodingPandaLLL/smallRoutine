64c6c08370e10a1991aa370cbd40e21a0a0c726d
lll <llli5900>
2020-08-17 13:05:37 +0800

    [M]提交代码

--- src/exercise/tx/Code.java
+++ src/exercise/tx/Code.java
@@ -1,4 +1,7 @@
 package exercise.tx;
+
+import java.util.Scanner;
+
 /*
 假定一种编码的编码范围是a ~ y的25个字母，从1位到4位的编码，如果我们把该编码按字典序排序，形成一个数组如下： a, aa, aaa, aaaa, aaab, aaac, … …, b, ba, baa, baaa, baab, baac … …, yyyw, yyyx,
  yyyy 其中a的Index为0，aa的Index为1，aaa的Index为2，以此类推。 编写一个函数，输入是任意一个编码，输出这个编码对应的Index.
@@ -15,6 +18,22 @@ baca
  */
 public class Code {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String code=sc.nextLine();
+        int[] base=new int[4];
+        for (int i = 3; i >=0; i--) {
+            if(i==3){
+                base[3]=1;
+            }else {
+                base[i]=base[i+1]*25+1;
+            }
+
+        }
+        int index=0;
+        for (int i = 0; i < code.length(); i++) {
+            index=index+(code.charAt(i)-'a')*base[i]+1;
+    }
+        System.out.println(index-1);
 
     }
 }

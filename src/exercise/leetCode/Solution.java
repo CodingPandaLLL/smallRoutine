fe8e1566b4cdb9a2d7a7b48e4bd766886f4162f3
CodingPandaLLL <lll_tony@163.com>
2022-07-11 23:35:36 +0800

    [feat][-]add leetCode code

--- src/exercise/leetCode/Solution.java
+++ src/exercise/leetCode/Solution.java
@@ -9,6 +9,9 @@ public class Solution {
         //初始化鏈表
         ListNode listNode1 = addListNode(listNode, 1);
         System.out.printf("" + listNode1);
+        //反向链表
+        System.out.printf("" + Solution2.reverseList(listNode1));
+
     }
 
     /**
@@ -32,6 +35,20 @@ public class Solution {
             return addListNode(newListNode, i);
         }
     }
+
+    public static ListNode reverseList(ListNode listNode) {
+        if (listNode.next == null) {
+            ListNode newListNode = null;
+            newListNode.next = listNode;
+            return newListNode;
+        } else {
+            ListNode newListNode = listNode.next;
+            newListNode.next = listNode;
+            return reverseList(newListNode);
+        }
+    }
+
+
 }
 
 
@@ -87,14 +104,75 @@ class Solution1 {
  * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
  */
 class Solution2 {
-    public ListNode reverseList(ListNode head) {
-        ListNode pre = null, next = null;
-        while (head != null) {
-            next = head.next;
-            head.next = pre;
-            pre = head;
-            head = next;
+    public static ListNode reverseList(ListNode head) {
+//        ListNode pre = null, next = null;
+//        while (head != null) {
+//            next = head.next;
+//            head.next = pre;
+//            pre = head;
+//            head = next;
+//        }
+//        return pre;
+        if (head == null || head.next == null) {
+            return head;
         }
-        return pre;
+        ListNode node = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return node;
+    }
+}
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
+/**
+ * 请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 示例 2：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：head = [[1,1],[2,1]]
+ * 输出：[[1,1],[2,1]]
+ * 示例 3：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：head = [[3,null],[3,0],[3,null]]
+ * 输出：[[3,null],[3,0],[3,null]]
+ * 示例 4：
+ * <p>
+ * 输入：head = []
+ * 输出：[]
+ * 解释：给定的链表为空（空指针），因此返回 null。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/fu-za-lian-biao-de-fu-zhi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author: lll
+ * @date: 2022年07月11日 23:07:27
+ */
+class Solution3 {
+    public Node copyRandomList(Node head) {
+  return null;
     }
 }
